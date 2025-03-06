bdcbc7c5af5a101c7e88c7c50966e907b7ef36f9
Benjamin Auer <benjamin.m.auer@nasa.gov>
2025-03-06 11:45:28 -0500

    more updates and cleanup

--- setup_env.sh
+++ setup_env.sh
@@ -1,9 +1,6 @@
 export GOWASP_PATH=/home/bmauer/swdev/packages/GOWASP
-#export GOWASP_GSIPATH=/home/dao_ops/GEOSadas-5_29_5_SLES15/GEOSadas/install-SLES15
-#export GOWASP_BUFRBIN=/home/dao_ops/GEOSadas-5_29_5_SLES15/GEOSadas/install-SLES15/bin
-export GOWASP_GSIPATH=/home/dao_ops/GEOSadas-5_29_5_SLES15/GEOSadas
-#export GOWASP_BUFRBIN=/home/dao_ops/GEOSadas-5_29_5_SLES15/GEOSadas/Linux/bin
+export GOWASP_GSIPATH=/home/dao_ops/GEOSadas-5_29_5_SLES15/GEOSadas/install-SLES15
 export GOWASP_G5MODS=/home/dao_ops/GEOSadas-5_29_5_SLES15/GEOSadas/install-SLES15/bin/g5_modules.sh
-export GEOSPATH1=$GOWASP_GSIPATH
 export BASEDIR1=$BASEDIR
+export GEOSINSTALL=$GOWASP_GSIPATH
 source $GOWASP_G5MODS
